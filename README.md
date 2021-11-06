b18b556b42ffd17714609982fc33bf0da63fc6bd
Harry Emeric <harryemeric@gmail.com>
2021-11-06 18:09:13 -0400

    Added install script

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Setup
 
-`source setup/sh`
+`source install.sh`
+`source setup.sh`
 
 This is definitely the nerdiest thing I've ever done
---
+++ install.sh
@@ -0,0 +1,5 @@
+if [ ! -f $HOME/.oh-my-zsh ]
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+fi
