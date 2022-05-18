2be71dc86dd74680fb54b3cce430cbb538803746
Harry Emeric <harryemeric@gmail.com>
2022-05-18 12:05:54 -0400

    Correct typo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 if [ ! -f $HOME/.oh-my-zsh ]
+then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 fi
