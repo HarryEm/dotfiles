ecafcd8c05afc67b241d46749450aba978d74864
Harry Emeric <harryemeric@gmail.com>
2025-04-09 18:40:37 +0800

    Install autosuggestions

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 if [ ! -d $HOME/.oh-my-zsh ]
 then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
+
+# Assuming you have git installed
+# If not, simply do "sudo yum install git -y"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
